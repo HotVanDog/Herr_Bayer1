680f1fc0dd25101c63619b73bf50e6b2acc0c23d
HotVanDog <132818425+HotVanDog@users.noreply.github.com>
2023-08-08 23:49:34 +0200

    andrew tate

--- Herr Bayer!/Assets/Herr Bayer.cs
+++ Herr Bayer!/Assets/Herr Bayer.cs
@@ -6,10 +6,14 @@ public class HerrBayer : MonoBehaviour
 {
     public float speed = 30;
     public float richtung;
+    private Rigidbody2D rb;
+    public float jumpH = 20;
+    private bool amBoden = false;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,5 +21,18 @@ public class HerrBayer : MonoBehaviour
     {
         richtung = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && amBoden == true)
+        {
+            rb.AddForce(Vector2.up * jumpH, ForceMode2D.Impulse);
+            amBoden=false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Boden")
+        {
+            amBoden = true;
+        }
     }
 }
